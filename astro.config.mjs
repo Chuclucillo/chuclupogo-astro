781cb9e0301846b712e447f05b7fb2c527ba351e
Chuclucillo <chuclucillo@gmail.com>
2025-03-02 19:08:35 +0100

    Actualizando features de Astrojs 5.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
@@ -10,10 +10,19 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://chuclupogo.dedomingo.net',
   integrations: [tailwind(), sitemap(), react()],
+
   output: 'server',
   adapter: cloudflare(),
   prefetch: {
     defaultStrategy: 'viewport',
     prefetchAll: true
-  }
+  },
+
+  env: {
+    schema: {
+      XATA_API_KEY: envField.string({ context: "server", access: "secret" }),
+      XATA_BRANCH: envField.string({ context: "server", access: "secret" }),
+      DATABASE_URL: envField.string({ context: "server", access: "secret" }),
+    }
+  },
 });
